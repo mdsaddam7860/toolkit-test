85a83358a25386879341b029f4be7ffbdecd11d2
Md Saddam <mdsonu7860@outlook.com>
2026-01-16 16:49:21 +0530

    test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { app } from "./app.js";
-import { testFunction } from "./controllers/test.controller.js";
+// import { testFunction } from "./controllers/test.controller.js";
 
 const PORT = process.env.PORT || 5000;
 
